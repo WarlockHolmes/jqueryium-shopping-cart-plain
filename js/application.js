f0f9f43ec3d3ccda8e422d30097b64f6e1253525
WarlockHolmes <grau.morgan@gmail.com>
2020-09-26 14:39:07 -0600

    Item total
    Calculates item total based on quantity input for each item.

--- js/application.js
+++ js/application.js
@@ -1,8 +1,24 @@
-var changeText = function (element, string) {
-  $(element).text(string);
+var updateItemTotal = function (row) {
+  var total = row.children('.item-total');
+  var quantity = Number(row.find('.item-quantity input').val());
+  var price = Number(row.children('.item-price').text());
+  var amount = quantity * price;
+  total.text(amount);
+  return amount;
 }
 
+
 $(document).ready(function(){
-  var target = $('td').first();
-  changeText(target, 'test');
+
+  $('tbody tr').each(function(i, row){
+    updateItemTotal($(row));
+  })
+
+  var timeout;
+  $(document).on('input', '.item-quantity input', function(){
+    clearTimeout(timeout);
+    timeout = setTimeout(function () {
+      updateItemTotal();
+    }, 1000);
+  })
 });
