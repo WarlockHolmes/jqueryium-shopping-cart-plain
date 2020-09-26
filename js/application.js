1197a4e6b0b290d039894a1dad04c9af37f40635
WarlockHolmes <grau.morgan@gmail.com>
2020-09-26 15:09:01 -0600

    Add new items
    user can add new items

--- js/application.js
+++ js/application.js
@@ -1,4 +1,4 @@
-var updateItemTotal = function (row) {
+var itemTotal = function (row) {
   var total = row.children('.item-total');
   var quantity = Number(row.find('.item-quantity input').val());
   var price = Number(row.children('.item-price').text());
@@ -7,18 +7,38 @@ var updateItemTotal = function (row) {
   return amount;
 }
 
+var updateItemTotal = function () {
+  $('tbody tr').each(function(i, row){
+    itemTotal($(row));
+  })
+}
 
 $(document).ready(function(){
 
-  $('tbody tr').each(function(i, row){
-    updateItemTotal($(row));
-  })
+  updateItemTotal();
 
   var timeout;
   $(document).on('input', '.item-quantity input', function(){
     clearTimeout(timeout);
     timeout = setTimeout(function () {
       updateItemTotal();
-    }, 1000);
+    }, 500);
   })
+
+  $(document).on('submit', '#add-item', function(){
+    event.preventDefault();
+    var item = $('#add-item input').first().val();
+    var price = $('#add-item input').last().val();
+    if (item !== undefined && name !== undefined){
+      $('tbody').append('<tr>'+
+        '<td class="item-name">'+item+'</td>'+
+        '<td class="item-price">'+price+'</td>'+
+        '<td class="item-quantity"><input name="name" type="number" value="1"/></td>'+
+        '<td class="item-total"></td>'+
+      '</tr>');
+      $('#add-item input').first().val('');
+      $('#add-item input').last().val('');
+    }
+    updateItemTotal();
+  });
 });
